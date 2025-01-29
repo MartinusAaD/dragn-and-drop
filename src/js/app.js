2336ca42cf1442bdfe5e604ef16ccf44e29a0b33
Martinus Aamot Dahl <maraad97@gmail.com>
2025-01-29 11:20:04 +0100

    Create List Items

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+import createListItems from "./createListItems.js";
+import stepsForSoftwareProject from "./stepsData.js";
+
+document.addEventListener("DOMContentLoaded", createListItems);
---
+++ src/js/createListItems.js
@@ -0,0 +1,26 @@
+import stepsForSoftwareProject from "./stepsData.js";
+
+const createListItems = () => {
+  const list = document.querySelector(".list");
+  list.innerHTML = "";
+
+  stepsForSoftwareProject.forEach((step, index) => {
+    const listItem = document.createElement("li");
+    list.append(listItem);
+    listItem.textContent = `${index + 1}. ${step}`;
+    listItem.classList.add("list__item");
+    listItem.setAttribute("draggable", true);
+    listItem.setAttribute("data-index", index);
+  });
+  const compareButton = document.createElement("button");
+  const mainContainer = document
+    .querySelector(".main-container")
+    .append(compareButton);
+  compareButton.textContent = "Check Order";
+  compareButton.classList.add("list__compare-button");
+
+  //   compareButton.addEventListener("click", compareOrder)
+  //   handleDragAndDrop();
+};
+
+export default createListItems;
